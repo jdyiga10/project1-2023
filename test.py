212701cb20416a997967407d19e033ced6230d4b
proman2520 <andrew.prozorovsky@gmail.com>
2023-08-08 00:07:05 -0500

    with imports

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import billboard
+import pandas as pd
+import matplotlib as plt
 
 #If this runs for you and shows the top 50 alternative songs of 2022, then your import and pip install work successfully
 #and we should be good to go as far as manipulating the data and extrapolating some interesting conclusions.
@@ -6,4 +8,7 @@ chart = billboard.ChartData('hot-100')
 print(chart.title)
 
 testchart = billboard.ChartData('alternative-songs', year=2022)
+print(testchart)
+
+testchart = billboard.ChartData('rap-songs', year=2022)
 print(testchart)
